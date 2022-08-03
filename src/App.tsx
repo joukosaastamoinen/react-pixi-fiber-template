2a954e0f7a68222f731ed1570874fc1bd2825c3c
Jouko Saastamoinen <joukosaastamoinen@gmail.com>
2022-08-03 22:57:20 +0300

    Eject

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { Texture } from "pixi.js";
 import { Sprite, Stage } from "react-pixi-fiber";
 import bunny from "./bunny.png";
 
+const OPTIONS = { backgroundColor: 0x10bb99, height: 600, width: 800 };
+
 function App() {
   return (
-    <Stage options={{ backgroundColor: 0x10bb99, height: 600, width: 800 }}>
+    <Stage options={OPTIONS}>
       <Sprite texture={Texture.from(bunny)} x={200} y={200} />
     </Stage>
   );
