e834a6296525bb2c29ea0d6e30fa4aacbadfacf8
Jouko Saastamoinen <joukosaastamoinen@gmail.com>
2022-08-04 15:42:29 +0300

    Add a popping sound on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { Texture } from "pixi.js";
 import { Sprite, Stage } from "@saitonakamura/react-pixi";
+import useSound from "use-sound";
 import bunny from "./bunny.png";
+import pop from "./pop.mp3";
 
 function App() {
+  const [playPop] = useSound(pop);
   return (
-    <Stage options={{ backgroundColor: 0x10bb99 }} width={800} height={600}>
-      <Sprite texture={Texture.from(bunny)} x={200} y={200} />
-    </Stage>
+    <div
+      onClick={() => {
+        playPop();
+      }}
+    >
+      <Stage options={{ backgroundColor: 0x10bb99 }} width={800} height={600}>
+        <Sprite texture={Texture.from(bunny)} x={200} y={200} />
+      </Stage>
+    </div>
   );
 }
 
